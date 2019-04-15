1364a634ae3c9f72a7d443e86a184d31b5ba745b
songtao <songtao_st08@163.com>
2019-04-15 17:12:14 +0800

    grow-to-express

--- example/index.js
+++ example/index.js
@@ -2,9 +2,9 @@ const express = require('../lib/express');
 const app = express();
 
 // send GET request
-app.get('/home', function(req, res) {
-    res.end('you send request throw get');
-})
+// app.get('/home', function(req, res) {
+//     res.end('you send request throw get');
+// })
 
 app.listen(3000, function(){
     console.log('app is listen on 3000')
