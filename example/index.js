020d6dc7b4dd61359bd093b3cb3025f8b20b6a23
songtao <songtao_st08@163.com>
2019-04-15 16:41:32 +0800

    第二次迭代，暂时是有问题的

--- example/index.js
+++ example/index.js
@@ -1,10 +1,17 @@
 const express = require('../lib/express');
 const app = express();
 
+app.post('/home', function(req, res) {
+    console.log('this is two')
+    res.end('you send request two throw post');
+})
+
 // send GET request
 app.get('/home', function(req, res) {
+    console.log('this is one')
     res.end('you send request throw get');
 })
+
 
 app.listen(3000, function(){
     console.log('app is listen on 3000')
