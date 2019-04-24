a5dcb6686b4fd295bb510e9bade037944b129ad3
songtao <songtao_st08@163.com>
2019-04-24 16:15:58 +0800

    支持创建web服务器

--- example/index.js
+++ example/index.js
@@ -1,17 +1,23 @@
 const express = require('../lib/express');
 const app = express();
+// var router = express.Router();
 
-app.get('/mo', function(req, res) {
-    console.log('this is two')
-    res.end('you send request two throw post');
-})
+// app.get('/mo', function(req, res) {
+//     console.log('this is two')
+//     res.end('you send request two throw post');
+// })
 
 // send GET request
-app.get('/home', function(req, res) {
-    console.log('this is one')
-    res.end('you send request throw get');
-})
+// app.get('/home', function(req, res) {
+//     console.log('this is one')
+//     res.end('you send request throw get');
+// })
 
+// router.use('/2', function(req, res, next) {
+// 	res.send('second user');
+// });
+
+// app.use('/users', router);
 
 app.listen(3000, function(){
     console.log('app is listen on 3000')
